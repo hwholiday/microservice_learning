384c3846cd69f319218a13f6d0e5d5b297029d9d
hwholiday <ai19950306>
2018-08-24 09:42:50 +0800

    修改配置文件

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@ nohup etcd >/dev/null 2>&1 &
 echo "启动etcd成功"
 nohup nsqlookupd >/dev/null 2>&1 &
 echo "启动nsqlookupd成功"
-nohup nsqd --lookupd-tcp-address=127.0.0.1:4160 -tcp-address=0.0.0.0:4152 -http-address=0.0.0.0:4153 >/dev/null 2>&1 &
-echo "启动nsqd成功"
 nohup nsqd --lookupd-tcp-address=127.0.0.1:4160 >/dev/null 2>&1 &
 echo "启动nsqd成功"
 nohup nsqadmin --lookupd-http-address=127.0.0.1:4161 >/dev/null 2>&1 &
